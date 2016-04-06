0c29e194b9076068906b362ffb4e277f39cb9d6e
Audrey Cole <audreycole88@yahoo.com>
2016-04-06 16:10:30 -0500

    Added sound and chrome notification functions to run in the background

--- UI/background.js
+++ UI/background.js
@@ -1,4 +1,4 @@
-// Set up context menu at install time.
+/*// Set up context menu at install time.
 chrome.runtime.onInstalled.addListener(function() {
   var context = "selection";
   var title = "Add Text/Image to Remember-Me";
@@ -13,9 +13,20 @@ chrome.contextMenus.onClicked.addListener(onClickHandler);
 function onClickHandler(info, tab) {
   var sText = info.selectionText;
   
-};
+};*/
 
-// Add to Remember Me
-function add_URL_RememberMe(text) {
-	
+createNotification("Haley", "is the best!");
+audioNotification();
+
+function audioNotification(){
+    var yourSound = new Audio('http://www.html5rocks.com/en/tutorials/audio/quick/test.mp3');
+    yourSound.play();
+}
+
+function createNotification(title, message){
+    var opt = {type: "basic",title: title,message: message,iconUrl: "UI/RMicon.png"}
+    chrome.notifications.create("notificationName",opt,function(){});
+
+    //include this line if you want to clear the notification after 5 seconds
+    setTimeout(function(){chrome.notifications.clear("notificationName",function(){});},5000);
 }
